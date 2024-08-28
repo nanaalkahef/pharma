e57e75d02959c3aed71b707e53273124730699ef
nanaalkahef <nana@gamil.com>
2024-08-28 08:36:41 -0700

    first commit

--- lib/view/screen/cart.dart
+++ lib/view/screen/cart.dart
@@ -1,10 +1,7 @@
-import 'dart:convert';
 
 import 'package:flutter/material.dart';
-import 'package:flutter/widgets.dart';
 import 'package:get/get.dart';
 
-import 'package:http/http.dart' as http;
 import 'package:pharma_mvc/controller/cart_controller.dart';
 import 'package:pharma_mvc/view/widget/faild.dart';
 
